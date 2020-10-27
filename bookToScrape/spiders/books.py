f7d34f2af1bd391525ff5864cf7a42091be4adf4
himson tam <himson.tam@bbpos.com>
2020-10-27 03:38:41 -0500

    spider script

--- bookToScrape/spiders/books.py
+++ bookToScrape/spiders/books.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# # -*- coding: utf-8 -*-
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
@@ -7,15 +7,54 @@ from scrapy.spiders import CrawlSpider, Rule
 class BooksSpider(CrawlSpider):
     name = 'books'
     allowed_domains = ['books.toscrape.com']
-    start_urls = ['http://books.toscrape.com/']
+    # start_urls = ['http://books.toscrape.com/']
+
+    # User agent of your currect version of chrome
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'
+
+    def start_requests(self):
+        yield scrapy.Request(url='http://books.toscrape.com/', headers={
+            'User-Agent': self.user_agent
+        })
 
     rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(restrict_xpaths="//div//article[@class='product_pod']"), callback='parse_item', follow=True, process_request='set_user_agent'),
+        Rule(LinkExtractor(restrict_xpaths="//li[@class='next']/a"), process_request='set_user_agent')
     )
 
+    def set_user_agent(self, request):
+        request.headers['User-Agent'] = self.user_agent
+        return request
+
     def parse_item(self, response):
-        item = {}
-        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
-        #item['name'] = response.xpath('//div[@id="name"]').get()
-        #item['description'] = response.xpath('//div[@id="description"]').get()
-        return item
+        yield {
+            'book_name': response.xpath("//div[@class='col-sm-6 product_main']/h1/text()").get(),
+            'price': response.xpath("//div/p[@class='price_color']/text()").get(),
+            # 'user-agent': response.request.header['User-agent']
+        }
+
+
+#Solution :
+# -*- coding: utf-8 -*-
+#########################################################################
+# import scrapy
+# from scrapy.linkextractors import LinkExtractor
+# from scrapy.spiders import CrawlSpider, Rule
+
+
+# class BooksSpider(CrawlSpider):
+#     name = 'books'
+#     allowed_domains = ['books.toscrape.com']
+#     start_urls = ['http://books.toscrape.com']
+
+#     rules = (
+#         Rule(LinkExtractor(restrict_xpaths="//article[@class='product_pod']/div/a"), callback='parse_item', follow=True),
+#         Rule(LinkExtractor(restrict_xpaths="//li[@class='next']/a"))
+#     )
+
+#     def parse_item(self, response):
+#         yield {
+#             'title': response.xpath("//div[@class='col-sm-6 product_main']/h1/text()").get(),
+#             'price': response.xpath("(//div[@class='col-sm-6 product_main']/p)[1]/text()").get()
+#         }
+#########################################################################
